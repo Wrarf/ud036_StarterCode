dcf9727918269195758c385af217045f6fea6c3d
Matteo <matteodocci.dev@gmail.com>
2018-03-06 16:09:25 +0100

    Change variable names, add informations and delete the trailer method

--- media.py
+++ media.py
@@ -3,14 +3,14 @@ import webbrowser
 class Movie():
     """ This class provides a way to store movie related information """
     
-    valid_ratings = ["G", "PG", "PG-13", "R"]
-    
-    def __init__(self, movie_title, movie_storyline, poster_image,
-                 trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
-
-    def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+    def __init__(self, title, storyline, poster_image_url,
+                 youtube_trailer_url, genre, year, rating, cast, reviews):
+        self.title = title
+        self.storyline = storyline
+        self.poster_image_url = poster_image_url
+        self.youtube_trailer_url = youtube_trailer_url
+        self.genre = genre
+        self.year = year
+        self.rating = rating
+        self.cast = cast
+        self.reviews = reviews
