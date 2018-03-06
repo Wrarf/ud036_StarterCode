602b0106809311661bf329eb4e403dae007a2ee9
Matteo <matteodocci.dev@gmail.com>
2018-03-06 17:08:11 +0100

    Add director to the movie class and create movies objects

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,35 +1,85 @@
+# -*- coding: cp1252 -*-
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-
-spirited_away = media.Movie("Spirited Away",
-                     "Tale of the fanciful adventures of a ten-year-old girl named Chihiro, who discovers a secret world",
-                     "https://upload.wikimedia.org/wikipedia/en/d/db/Spirited_Away_Japanese_poster.png",
-                     "https://www.youtube.com/watch?v=ByXuk9QqQkk")
-
-the_truman_show = media.Movie("The Truman Show",
-                              "Truman Burbank is the unsuspecting star of The Truman Show, a reality television program which is broadcast live around the clock and across the globe.",
-                              "https://upload.wikimedia.org/wikipedia/en/c/cd/Trumanshow.jpg",
-                              "https://www.youtube.com/watch?v=loTIzXAS7v4")
-
-midnight_in_paris = media.Movie("Midnight in Paris",
-                                "Gil takes a walk at midnight and discovers what could be the ultimate source of inspiration for writing",
-                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
+standard_reviews = ["Nice film", "I liked it"]
 
 up = media.Movie("Up",
-                 "Carl Fredricksen as a boy wanted to explore South America and find the forbidden Paradise Falls. About 64 years later he gets to begin his journey along with a Boy Scout named Russel with help from 500 balloons.",
+                 "Pete Docter",
+                 "After the death of his wife and an incident at home, an old man, Carl Fredericksen, is forced into a retirement home. However, he still wants to go on the adventure to South America he and his wife planned.Ultimately, the adventure involves a flying house, a young boy, a talking dog and a strange, large bird.",
                  "https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg",
-                 "https://www.youtube.com/watch?v=kBBsE4TK06U")
+                 "https://www.youtube.com/watch?v=qas5lWp7_R0&t=1s",
+                 ["adventure", "animation", "family", "comedy"],
+                 "2009",
+                 5,
+                 [],
+                 standard_reviews)
 
-birdman = media.Movie("Birdman or (The Unexpected Virtue of Ignorance)",
-                      "A washed-up actor, who once played an iconic superhero, attempts to revive his career by writing and starring in his very own Broadway play",
-                      "https://upload.wikimedia.org/wikipedia/en/6/63/Birdman_poster.png",
-                      "https://www.youtube.com/watch?v=uJfLoE6hanc")
+howls_moving_castle = media.Movie("Howl's Moving Castel",
+                                  "Hayao Miyazaki",
+                                  "When an unconfident young woman is cursed with an old body by a spiteful witch, her only chance of breaking the spell lies with a self-indulgent yet insecure young wizard and his companions in his legged, walking castle.",
+                                  "https://fiu-assets-2-syitaetz61hl2sa.stackpathdns.com/static/use-media-items/17/16574/upto-700xauto/56702d32/Howls_Moving_Castle-Olly_Moss.jpeg?resolution=0",
+                                  "https://www.youtube.com/watch?v=iwROgK94zcM",
+                                  ["animation", "drama", "fantasy", "science fiction", "romance"],
+                                  "2005",
+                                  4,
+                                  [],
+                                  standard_reviews)
+
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "Woody Allen",
+                                "Gil takes a walk at midnight and discovers what could be the ultimate source of inspiration for writing",
+                                "https://eu.movieposter.com/posters/archive/main/145/MPW-72510",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY&t=1s",
+                                ["comedy", "romance"],
+                                "2011"
+                                5,
+                                ["Owen Wilson", "Rachel McAdams", "Kathy Bates"],
+                                standard_reviews)
+
+toy_story_3 = media.Movie("Toy Story 3",
+                          "Lee Unkrich",
+                          "Andy is going to college and doesn't play with his toys anymore causing Woody and the others to be donated to Sunnyside Daycare, where they discover that the place is not exactly what they dreamed it was, and will have to face many dangers to come home safely.",
+                          "http://images6.fanpop.com/image/photos/40100000/Toy-Story-3-The-Great-Escape-Poster-pixar-40152166-354-500.jpg",
+                          "https://www.youtube.com/watch?v=ZZv1vki4ou4",
+                          ["animation", "comedy"],
+                          "2010",
+                          5,
+                          [],
+                          standard_reviews)
+
+spotless_mind = media.Movie("Eternal Sunshine of the Spotless Mind",
+                            "Michel Gondry",
+                            "A man, Joel Barish, heartbroken that his girlfriend Clementine underwent a procedure to erase him from her memory, decides to do the same. However, as he watches his memories of her fade away, he realizes that he still loves her, and may be too late to correct his mistake.",
+                            "https://i.pinimg.com/564x/67/25/3c/67253cb97e3827cc3738c4140f9c9891.jpg",
+                            "https://www.youtube.com/watch?v=hj7Wgd_Yn94",
+                            ["comedy", "drama", "science fiction", "romance"],
+                            "2004",
+                            5,
+                            ["Jim Carrey", "Kate Winslet", "Kirsten Dunst", "Mark Ruffalo", "Elijah Wood", "Tom Wilkinson"],
+                            standard_reviews)
+
+grand_budapest_hotel = media.Movie("The Grand Budapest Hotel",
+                                   "Wes Anderson",
+                                   "The adventures of Gustave H, a legendary concierge at a famous hotel from the fictional Republic of Zubrowka between the first and second World Wars, and Zero Moustafa, the lobby boy who becomes his most trusted friend.",
+                                   "http://poolga.com/shared/images/poolgas/iphone_2032.jpg",
+                                   "https://www.youtube.com/watch?v=1Fg5iWmQjwk",
+                                   ["comedy", "drama"],
+                                   "2014",
+                                   4,
+                                   ["Ralph Fiennes", "Tony Revolori", "F. Murray Abraham"],
+                                   standard_reviews)
+
+birdman = media.Movie("Birdman or (The Unespected Virtue of Ignorance)",
+                      "Alejandro González Iñárritu",
+                      "A washed-up actor, who once played an iconic superhero, attempts to revive his career by writing and starring in his very own Broadway play.",
+                      "https://pre00.deviantart.net/6308/th/pre/f/2015/160/3/a/birdman_vector_poster_by_metalraj-d8wpn4q.jpg",
+                      "https://www.youtube.com/watch?v=uJfLoE6hanc&t=4s",
+                      ["comedy", "drama"],
+                      "2014",
+                      5,
+                      ["Michael Keaton", "Edward Norton", "Zach Galifianakis", "Emma Stone"],
+                      standard_reviews)
 
 movies = [toy_story, spirited_away, the_truman_show, midnight_in_paris, up, birdman]
-fresh_tomatoes.open_movies_page(movies)
+#fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -3,13 +3,14 @@ import webbrowser
 class Movie():
     """ This class provides a way to store movie related information """
     
-    def __init__(self, title, storyline, poster_image_url,
-                 youtube_trailer_url, genre, year, rating, cast, reviews):
+    def __init__(self, title, director, storyline, poster_image_url,
+                 youtube_trailer_url, genres, year, rating, cast, reviews):
         self.title = title
+        self.director = director
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.youtube_trailer_url = youtube_trailer_url
-        self.genre = genre
+        self.genres = genres
         self.year = year
         self.rating = rating
         self.cast = cast
