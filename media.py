ce0d0e1f39a8dcae12ec7867135c2738ab6d6cab
Matteo <matteodocci.dev@gmail.com>
2018-03-14 11:13:03 +0100

    Add description of the attributes' datatypes of the class Movie

--- media.py
+++ media.py
@@ -6,12 +6,12 @@ class Movie():
 
     def __init__(self, title, director, storyline, poster_image_url,
                  youtube_trailer_url, genres, year, rating, cast):
-        self.title = title
-        self.director = director
-        self.storyline = storyline
-        self.poster_image_url = poster_image_url
-        self.youtube_trailer_url = youtube_trailer_url
-        self.genres = genres
-        self.year = year
-        self.rating = rating
-        self.cast = cast
+        self.title = title  # string
+        self.director = director  # string
+        self.storyline = storyline  # string
+        self.poster_image_url = poster_image_url  # string
+        self.youtube_trailer_url = youtube_trailer_url  # string
+        self.genres = genres  # list of strings
+        self.year = year  # string
+        self.rating = rating  # int
+        self.cast = cast  # list of strings
